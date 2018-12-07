2d9aedcc74cff9969474737eca788d94ddb35a3a
陈嘉涵 <chenjiahan@youzan.com>
2018-12-07 15:47:13 +0800

    [build] add release script

---
+++ build/release.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  # build
+  npm version $VERSION --no-git-tag-version
+  VERSION=$VERSION npm run build
+
+  # commit
+  git tag v$VERSION
+  git commit -am "[release] $VERSION"
+
+  # publish
+  git push origin refs/tags/v$VERSION
+  git push origin master
+
+  npm publish
+fi
